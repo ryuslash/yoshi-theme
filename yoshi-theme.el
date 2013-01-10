f0af3f9f63e23708dc5baff5367e7a61a2b4dd84
Tom Willemsen <tom@ryuslash.org>
2013-01-10 04:16:28 +0100

    Shouldn't decrease version

--- yoshi-theme.el
+++ yoshi-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: Tom Willemsen <tom@ryuslash.org>
 ;; Keywords: faces
-;; Version: 1.1.0
+;; Version: 3.1.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by

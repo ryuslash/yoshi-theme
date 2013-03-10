726fb2b641f75e0d9c298f195aca0d1b952afb75
Tom Willemsen <tom@ryuslash.org>
2013-03-10 14:44:31 +0100

    Autoload theme and use rainbow-mode

--- yoshi-theme.el
+++ yoshi-theme.el
@@ -139,5 +139,16 @@
 
    ))
 
+;;;###autoload
+(and load-file-name
+     (boundp 'custom-theme-load-path)
+     (add-to-list 'custom-theme-load-path
+                  (file-name-as-directory
+                   (file-name-directory load-file-name))))
+
 (provide-theme 'yoshi)
 ;;; yoshi-theme.el ends here
+
+;; Local Variables:
+;; eval: (rainbow-mode 1)
+;; End:

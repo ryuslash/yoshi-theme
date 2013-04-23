7cf5c7436d282b2f9126eca93cb1b28d70ce3fe2
Tom Willemse <tom@ryuslash.org>
2013-04-23 22:48:05 +0200

    Fix versions

--- yoshi-theme.el
+++ yoshi-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: Tom Willemsen <tom@ryuslash.org>
 ;; Keywords: faces
-;; Version: 5.4.0.4
+;; Version: 5.5.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by

ebe939795543bfcf9b969c0540500fdfb322061e
Tom Willemse <tom@ryuslash.org>
2015-11-15 15:11:52 +0100

    Add URL

--- yoshi-theme.el
+++ yoshi-theme.el
@@ -5,6 +5,7 @@
 ;; Author: Tom Willemse <tom@ryuslash.org>
 ;; Keywords: faces
 ;; Version: 6.0.0
+;; URL: http://projects.ryuslash.org/yoshi-theme/
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by

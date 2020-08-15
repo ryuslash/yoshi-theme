d7354e148767313c7e61b565c2c32fba4ab6cd24
Tom Willemse <tom@ryuslash.org>
2020-08-15 10:51:17 -0700

    Let users specify the width, height, and depth for the screenshot

--- make-screenshot/entrypoint.sh
+++ make-screenshot/entrypoint.sh
@@ -1,3 +1,40 @@
 #!/usr/bin/env bash
 
-xvfb-run --server-args="-screen 0 896x576x24" /make-screenshot "$@"
+PARSED_ARGUMENTS=$(getopt --long 'width:,height:,depth:' -n 'entrypoint.sh' '' "$@")
+VALID_ARGUMENTS=$?
+
+if [ $VALID_ARGUMENTS -ne 0 ]; then
+    echo 'Terminating...' >&2
+    exit 1
+fi
+
+eval set -- "$PARSED_ARGUMENTS"
+
+while true; do
+    case "$1" in
+        --width)
+            screen_width="$2"
+            shift 2
+            ;;
+        --height)
+            screen_height="$2"
+            shift 2
+            ;;
+        --depth)
+            screen_depth="$2"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Unexpected option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+xvfb-run \
+    --server-args="-screen 0 ${screen_width}x${screen_height}x${screen_depth}" \
+    /make-screenshot "$@"

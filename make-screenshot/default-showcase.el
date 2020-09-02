c85027a44615d33846e5576b2dd75a618847316d
Tom Willemse <tom@ryuslash.org>
2020-09-02 15:43:54 -0700

    Try updating the UI instead of waiting

--- make-screenshot/default-showcase.el
+++ make-screenshot/default-showcase.el
@@ -1,6 +1,9 @@
 (defun showcase (working-directory)
   (princ "Turning off menu-bar-mode\n")
-  (sit-for 3)
+
+  (force-window-update)
+  (redisplay)
+
   (menu-bar-mode -1)
   (princ "Turning off tool-bar-mode\n")
   (tool-bar-mode -1)

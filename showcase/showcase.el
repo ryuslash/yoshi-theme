13d13ad3d4d4437877942b8756006329aa71191e
Tom Willemse <tom@ryuslash.org>
2020-09-07 14:27:47 -0700

    Bring back Haskell
    Now that the built-in Emacs Lisp and Ruby modes work, try making a screenshot
    with Haskell.

--- showcase/showcase.el
+++ showcase/showcase.el
@@ -1,13 +1,12 @@
 (defun showcase (working-directory)
-
-  ;; (package-refresh-contents)
-  ;; (package-install "haskell-mode")
+  (package-refresh-contents)
+  (package-install "haskell-mode")
 
   (find-file (expand-file-name "showcase/emacs-lisp-mode" working-directory))
   (make-screenshot (expand-file-name "screenshots/emacs-lisp.png" working-directory))
 
-  ;; (find-file "showcase/haskell-mode")
-  ;; (make-screenshot "screenshots/haskell.png")
+  (find-file "showcase/haskell-mode")
+  (make-screenshot "screenshots/haskell.png")
 
   (find-file (expand-file-name "showcase/ruby-mode" working-directory))
   (make-screenshot (expand-file-name "screenshots/ruby.png" working-directory)))

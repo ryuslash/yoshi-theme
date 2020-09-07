7cfd4da9be6811a56b97411866518fd15d9f7e64
Tom Willemse <tom@ryuslash.org>
2020-09-07 11:39:43 -0700

    Define the ‘showcase’ function as my action expects

--- showcase/showcase.el
+++ showcase/showcase.el
@@ -1,11 +1,13 @@
-;; (package-refresh-contents)
-;; (package-install "haskell-mode")
+(defun showcase (working-directory)
 
-(find-file "showcase/emacs-lisp-mode")
-(make-screenshot "screenshots/emacs-lisp.png")
+  ;; (package-refresh-contents)
+  ;; (package-install "haskell-mode")
 
-;; (find-file "showcase/haskell-mode")
-;; (make-screenshot "screenshots/haskell.png")
+  (find-file "showcase/emacs-lisp-mode")
+  (make-screenshot "screenshots/emacs-lisp.png")
 
-(find-file "showcase/ruby-mode")
-(make-screenshot "screenshots/ruby.png")
+  ;; (find-file "showcase/haskell-mode")
+  ;; (make-screenshot "screenshots/haskell.png")
+
+  (find-file "showcase/ruby-mode")
+  (make-screenshot "screenshots/ruby.png"))

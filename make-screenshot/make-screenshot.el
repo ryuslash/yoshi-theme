d4a096a21d900ad14c9ca6f782e4f57079e4811d
Tom Willemse <tom@ryuslash.org>
2020-09-07 14:02:35 -0700

    Handle errors from anywhere in the function

--- make-screenshot/make-screenshot.el
+++ make-screenshot/make-screenshot.el
@@ -21,30 +21,33 @@
                        (buffer-substring-no-properties (point-min) (point-max))))))))
 
 (defun make-screenshot-run (theme-name script)
-  (setq standard-output #'external-debugging-output)
+  (condition-case outer-err
+      (progn
+        (setq standard-output #'external-debugging-output)
 
-  (let* ((frame-resize-pixelwise t)
-         (default-directory (getenv "GITHUB_WORKSPACE"))
-         (script-file (expand-file-name script default-directory))
-         success)
-    (toggle-frame-maximized)
+        (let* ((frame-resize-pixelwise t)
+               (default-directory (getenv "GITHUB_WORKSPACE"))
+               (script-file (expand-file-name script default-directory))
+               success)
+          (toggle-frame-maximized)
 
-    (add-to-list 'custom-theme-load-path default-directory)
-    (princ (format "::debug::Loading theme: %s\n" theme-name))
-    (load-theme (intern theme-name) t)
+          (add-to-list 'custom-theme-load-path default-directory)
+          (princ (format "::debug::Loading theme: %s\n" theme-name))
+          (load-theme (intern theme-name) t)
+          (princ (format "::debug::Loaded theme: %s\n" theme-name))
 
-    (unwind-protect
-        (with-timeout (300 (kill-emacs 2))
-          (sit-for 3)
+          (unwind-protect
+              (with-timeout (300 (kill-emacs 2))
+                (sit-for 3)
 
-          (condition-case err
-              (progn
-                (load script-file)
-                (showcase default-directory))
-            (t
-             (princ (format "::error::Encountered error: %s" err))))
-          (setq success t))
-      (kill-emacs (if success 0 1))
-      (print (format "::debug::Message buffer contents: \n%s"
-                     (with-current-buffer (messages-buffer)
-                       (buffer-string)))))))
+                (condition-case err
+                    (progn
+                      (load script-file)
+                      (showcase default-directory))
+                  (t (princ (format "::error::Encountered error while running showcase: %s" err))))
+                (setq success t))
+            (kill-emacs (if success 0 1))
+            (print (format "::debug::Message buffer contents: \n%s"
+                           (with-current-buffer (messages-buffer)
+                             (buffer-string)))))))
+    (t (princ (format "::error::Encountered error: %s" outer-err)))))
